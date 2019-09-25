7313ef2e004e6826f52d6adc2bbd55115e676da2
Scott Jordan <scottjordanswimming@gmail.com>
2019-09-25 20:12:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,16 +3,12 @@ class Dog
     @this_dogs_name = dogs_name
 end
 
-def bark=(dogs_bark)
-  @this_dogs_bark = dogs_bark
-end
-
 def name
   @this_dogs_name
 end
 
 def bark
-  @this_dogs_bark
+  puts "woof!"
 end
 end
 
